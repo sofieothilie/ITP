c9e54e9fdd83129787d70cb26ef1d9c14ee617a9
elisacas <elisacas@stud.ntnu.no>
2022-09-21 11:16:33 +0200

    Fixed tests in User.java
    Changed the tests in user to after editing the class
    
    This fixes #27

--- movieRating/core/src/test/java/core/UserTest.java
+++ movieRating/core/src/test/java/core/UserTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
+import java.util.HashMap; 
 
 
 public class UserTest {
@@ -24,21 +24,21 @@ public class UserTest {
     @Test
     public void testGetRatedMovies(){
         System.out.println(m1.getMovieRegister());
-        testUser.rateMovie(m1, 4.0);
-        HashMap<Movie, Double> compare = new HashMap<>();
-        compare.put(m1, 4.0);
+        testUser.rateMovie(m1, 4);
+        HashMap<Movie, Integer> compare = new HashMap<>();
+        compare.put(m1, 4);
         assertEquals(compare, testUser.getRatedMovies());
     }
 
     @Test
     public void assertRateMovie(){
-        testUser.rateMovie(m1, 3.5);
+        testUser.rateMovie(m1, 3);
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            testUser.rateMovie(m1, 3.0);; 
+            testUser.rateMovie(m1, 4);; 
         }, "The Movie is already rated");
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            testUser.rateMovie(m2, 6.0);; 
+            testUser.rateMovie(m2, 6);; 
         }, "Rating must be an integer from 1 to 5");
 
         
