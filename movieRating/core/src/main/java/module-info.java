db4da6feaccf229be06b5afa1742847a17993862
sofieothilie <sofieothilie@gmail.com>
2022-10-07 11:52:24 +0200

    Removed unsure requirement in moduleinfo in core
    this fixes #46

--- movieRating/core/src/main/java/module-info.java
+++ movieRating/core/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module movieRating.core {
     exports core;
     requires com.fasterxml.jackson.core;
-    requires com.fasterxml.jackson.databind;
     //requires com.fasterxml.jackson.core.JsonGenerator;
 }
