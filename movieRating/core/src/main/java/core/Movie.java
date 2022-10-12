cfb6abeea11fefafbec7d9eeea9dc0034f8063a6
sofieothilie <sofieothilie@gmail.com>
2022-10-12 22:12:55 +0200

    Removes unused method
    this fixes #95

--- movieRating/core/src/main/java/core/Movie.java
+++ movieRating/core/src/main/java/core/Movie.java
@@ -66,11 +66,8 @@ public class Movie {
     }
 
     public String toString(){ //returnerer en string med tittel og sjanger og gjennomsnittsrating
-        return ""+ this.getTitle() + "; " + this.getGenre() + "; " + String.format("%.2f",averageRating());
+        return ""+ this.getTitle() + "; " + this.getGenre() + "; " + String.format("%.2f",this.averageRating());
     }
 
-    public String movieInfoString(){
-        return ""+ this.getTitle() + "; " + this.getGenre() + "; " + getAllRatings();
-    }
 }
 
