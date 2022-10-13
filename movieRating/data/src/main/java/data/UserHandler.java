d70f6288429567985852e9ceb5c73707bbd16401
sofieothilie <sofieothilie@gmail.com>
2022-10-13 11:36:00 +0200

    Fixes bugs in user handler
    this fixes #105

--- movieRating/data/src/main/java/data/UserHandler.java
+++ movieRating/data/src/main/java/data/UserHandler.java
@@ -1,6 +1,5 @@
 package data;
 
-import core.Movie;
 import core.User;
 
 import java.io.IOException;
@@ -9,7 +8,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 
@@ -37,6 +38,7 @@ public class UserHandler {
             users.add(user);
             ObjectMapper objectMapper = new ObjectMapper();
             ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter()); 
+            
             objectWriter.writeValue(getFile(), users);
         }
         catch (IOException e){
@@ -62,24 +64,48 @@ public class UserHandler {
         }
     }
 
-    public void updateRegister(User user){
+    public void updateRegister(User user){        
+        try {
+            //Takes in a user and a rating and updates it in the register
+            //Generates a list of all user objects in file:
+            User oldUserToRemove = this.userExists(user);
+            List<User> userList = this.readUsersFromRegister();
+            userList.remove(oldUserToRemove);
+            userList.add(user);
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter()); 
+            objectWriter.writeValue(getFile(), userList);
+
+        } catch (JsonGenerationException e) {
+            throw new IllegalStateException("JsonGeneration Exception");
+        } catch (JsonMappingException e) {
+            throw new IllegalStateException("JsonMapping Exception");
+        } catch (IOException e) {
+            throw new IllegalStateException("IOException Exception");
+        }
+
+    }
+
+    private User userExists(User user){
         //Takes in a user and a rating and updates it in the register
 
         //Generates a list of all user objects in file:
         List<User> userList = this.readUsersFromRegister();
 
         //Writes all previous users and the new updates user to file:
-        if (!userList.contains(user)){
+        User oldUserToRemove = null;
+        for (User oldUser : userList) {
+            if (oldUser.getUsername().equals(user.getUsername()) && oldUser.getPassword().equals(user.getPassword())){
+                oldUserToRemove = oldUser;
+            }    
+        }
+        if (oldUserToRemove == null){
             throw new IllegalArgumentException("no such user");
         }
-        else{
-            for (User oldUser : userList) {
-                if (oldUser.getUsername().equals(user.getUsername()) && oldUser.getPassword().equals(user.getPassword())){
-                    userList.remove(oldUser);
-                    userList.add(user);
-                }    
-            }
-        }
+
+        return oldUserToRemove;
+
     }
 
     public boolean fileExists(){
@@ -91,5 +117,4 @@ public class UserHandler {
             return false;
         }
     }
-
 }
