494197621e00bb2382ecc802709872d2a85d456c
Thea Slemdal Bergersen <thesb@stud.ntnu.no>
2022-10-25 10:47:54 +0200

    149 Fix App Name
    Fixed App Name, this fixes issue #149

--- movieRating/ui/src/main/java/ui/MovieRatingApp.java
+++ movieRating/ui/src/main/java/ui/MovieRatingApp.java
@@ -15,7 +15,7 @@ public class MovieRatingApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        primaryStage.setTitle("MovieRatingApp"); //TODO: legge inn mellomrom
+        primaryStage.setTitle("Movie Rating App"); //TODO: legge inn mellomrom
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MovieRating.fxml"))));
         primaryStage.show();
     }
