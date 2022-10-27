26d896d094ae5a3945b33b404ae0a0870d122768
elisacas <elisacas@stud.ntnu.no>
2022-10-27 10:57:06 +0200

    Fix indentation wanrings
    Fixes indentation warning in UserHandler.java
    Fixes #164

--- movieRating/data/src/main/java/data/UserHandler.java
+++ movieRating/data/src/main/java/data/UserHandler.java
@@ -1,121 +1,118 @@
 package data;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import core.User;
-
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-
-import java.io.File;
-
 public class UserHandler {
 
-    public static final String SAVE_FOLDER = "/movieRating/data/src/main/java/data/";
-    public static final String fileName = "UserRegister.json";
+  public static final String SAVE_FOLDER =
+    "/movieRating/data/src/main/java/data/";
+  public static final String fileName = "UserRegister.json";
 
-    //TODO: add System.getProperty("user.home")
-    public File getFile(){ //TODO: try to fix Path
-        //Returns file
-        //String path = Paths.get(".").toAbsolutePath().normalize().toString();
-        //return new File(path + SAVE_FOLDER + fileName);
-        return new File(fileName);
+  //TODO: add System.getProperty("user.home")
+  public File getFile() { //TODO: try to fix Path
+    //Returns file
+    //String path = Paths.get(".").toAbsolutePath().normalize().toString();
+    //return new File(path + SAVE_FOLDER + fileName);
+    return new File(fileName);
+  }
+
+  public void writeUserToRegister(User user) {
+    //Writes user to file with JSON.
+    //UserRegister secures that duplicate user aren't written to file.
+    try {
+      List<User> users = new ArrayList<User>();
+      if (this.fileExists()) {
+        users = readUsersFromRegister();
+      }
+      users.add(user);
+      ObjectMapper objectMapper = new ObjectMapper();
+      ObjectWriter objectWriter = objectMapper.writer(
+        new DefaultPrettyPrinter()
+      );
+
+      objectWriter.writeValue(getFile(), users);
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Error: " + e);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Error: " + e);
     }
+  }
 
-    public void writeUserToRegister(User user){
-        //Writes user to file with JSON.
-        //UserRegister secures that duplicate user aren't written to file.
-        try {
-            List<User> users = new ArrayList<User>();
-            if (this.fileExists()){
-                users = readUsersFromRegister();
-            }
-            users.add(user);
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter()); 
-            
-            objectWriter.writeValue(getFile(), users);
-        }
-        catch (IOException e){
-            throw new IllegalArgumentException("Error: " + e);
-        }
-        catch (Exception e){
-            throw new IllegalArgumentException("Error: " + e);
-        }   
+  public List<User> readUsersFromRegister() {
+    //Reads from file and generates a list of users based on it.
+    try {
+      // create object mapper instance
+      ObjectMapper mapper = new ObjectMapper();
+
+      // convert JSON array to list of users
+      List<User> users = Arrays.asList(
+        mapper.readValue(getFile(), User[].class)
+      );
+      return new ArrayList<>(users);
+    } catch (Exception e) {
+      throw new IllegalArgumentException("Error: " + e);
     }
+  }
 
-    public List<User> readUsersFromRegister(){
-        //Reads from file and generates a list of users based on it.
-        try {
-            // create object mapper instance
-            ObjectMapper mapper = new ObjectMapper();
+  public void updateRegister(User user) {
+    try {
+      //Takes in a user and a rating and updates it in the register
+      //Generates a list of all user objects in file:
+      User oldUserToRemove = this.userExists(user);
+      List<User> userList = this.readUsersFromRegister();
+      userList.remove(oldUserToRemove);
+      userList.add(user);
 
-            // convert JSON array to list of users
-            List<User> users = Arrays.asList(mapper.readValue(getFile(), User[].class));
-            return new ArrayList<>(users);
-        }
-        catch (Exception e){
-            throw new IllegalArgumentException("Error: " + e);
-        }
+      ObjectMapper objectMapper = new ObjectMapper();
+      ObjectWriter objectWriter = objectMapper.writer(
+        new DefaultPrettyPrinter()
+      );
+      objectWriter.writeValue(getFile(), userList);
+    } catch (JsonGenerationException e) {
+      throw new IllegalStateException("JsonGeneration Exception");
+    } catch (JsonMappingException e) {
+      throw new IllegalStateException("JsonMapping Exception");
+    } catch (IOException e) {
+      throw new IllegalStateException("IOException Exception");
     }
+  }
 
-    public void updateRegister(User user){        
-        try {
-            //Takes in a user and a rating and updates it in the register
-            //Generates a list of all user objects in file:
-            User oldUserToRemove = this.userExists(user);
-            List<User> userList = this.readUsersFromRegister();
-            userList.remove(oldUserToRemove);
-            userList.add(user);
+  private User userExists(User user) {
+    //Takes in a user and a rating and updates it in the register
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectWriter objectWriter = objectMapper.writer(new DefaultPrettyPrinter()); 
-            objectWriter.writeValue(getFile(), userList);
+    //Generates a list of all user objects in file:
+    List<User> userList = this.readUsersFromRegister();
 
-        } catch (JsonGenerationException e) {
-            throw new IllegalStateException("JsonGeneration Exception");
-        } catch (JsonMappingException e) {
-            throw new IllegalStateException("JsonMapping Exception");
-        } catch (IOException e) {
-            throw new IllegalStateException("IOException Exception");
-        }
-
+    //Writes all previous users and the new updates user to file:
+    User oldUserToRemove = null;
+    for (User oldUser : userList) {
+      if (oldUser.equals(user)) {
+        oldUserToRemove = oldUser;
+      }
     }
-
-    private User userExists(User user){
-        //Takes in a user and a rating and updates it in the register
-
-        //Generates a list of all user objects in file:
-        List<User> userList = this.readUsersFromRegister();
-
-        //Writes all previous users and the new updates user to file:
-        User oldUserToRemove = null;
-        for (User oldUser : userList) {
-            if (oldUser.equals(user)){
-                oldUserToRemove = oldUser;
-            }    
-        }
-        if (oldUserToRemove == null){
-            throw new IllegalArgumentException("no such user");
-        }
-        return oldUserToRemove;
-
+    if (oldUserToRemove == null) {
+      throw new IllegalArgumentException("no such user");
     }
+    return oldUserToRemove;
+  }
 
-    public boolean fileExists(){ // check if the file exists
-        File f = new File(getFile().getAbsolutePath());
-        if (f.isFile()){
-            return true;
-        }
-        else{
-            return false;
-        }
+  public boolean fileExists() { // check if the file exists
+    File f = new File(getFile().getAbsolutePath());
+    if (f.isFile()) {
+      return true;
+    } else {
+      return false;
     }
+  }
 }
