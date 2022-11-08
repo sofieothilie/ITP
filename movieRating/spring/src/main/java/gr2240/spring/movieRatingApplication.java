f279b61cd4979e4f9b80ffc8e2c7628bddac1af4
sofieothilie <sofieothilie@gmail.com>
2022-11-08 15:37:07 +0100

    Splits application and controller to two files
    Co-authored-by: Thea <thesb@stud.ntnu.no>
    
    fixes #173

--- movieRating/spring/src/main/java/gr2240/spring/movieRatingApplication.java
+++ movieRating/spring/src/main/java/gr2240/spring/movieRatingApplication.java
@@ -1,25 +1,10 @@
 package gr2240.spring;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class movieRatingApplication {
-    @RequestMapping("/")
-    String home() {
-        return "Hello Worldsssss!";
-    }
-
-    // hente filmer
-    // 
-    // Movie getMovies() {
-    //     // bruk lokal lagring til å hente filmer, og returner de som json til javafx klienten
-    // }
-
-
     public static void main(String[] args) {
         SpringApplication.run(movieRatingApplication.class, args);
     }
