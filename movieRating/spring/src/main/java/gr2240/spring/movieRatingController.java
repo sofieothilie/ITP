7da95f3b57eff476e0936aa87874db9a76931c68
sofieothilie <sofieothilie@gmail.com>
2022-11-09 15:11:28 +0100

    Implements more controller functionality in spring
    Co-authored-by: Thea Slemdal Bergersen <thesb@stud.ntnu.no>
    fixes #173

--- movieRating/spring/src/main/java/gr2240/spring/movieRatingController.java
+++ movieRating/spring/src/main/java/gr2240/spring/movieRatingController.java
@@ -1,11 +1,15 @@
 package gr2240.spring;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import core.Movie;
-import data.MovieRegister;
+import core.User;
 
 /**
  * Controller class for MovieRatingApplication.
@@ -14,36 +18,59 @@ import data.MovieRegister;
 @RestController
 @RequestMapping("/movieRating")
 public class MovieRatingController {
-    /**
-     * Fields used in class.
-     */
-    private MovieRegister movieRegister = new MovieRegister("movieRegistry");
 
-    /**
-     * Writes movies to localhost:8080/movieRating/movies.
-     * @return List<Movie>
-     */
-    @RequestMapping(path = "movies")
-    public List<Movie> getMovieRegister() {
-        return movieRegister.updateMovieList();
+  private MovieRegisterService movSer;
+  private UserRegisterService userSer; 
+
+  public MovieRatingController(){
+      this.movSer = new MovieRegisterService();
+      this.userSer = new UserRegisterService();
+  }
+
+  /**
+   * Writes movies to localhost:8080/movieRating/movies.
+   * @return List<Movie>
+   */
+  @RequestMapping(path = "movies")
+  public Movie getMovieRegister() {
+      return new Movie ("Titanic", "fantasy");
+  }
+
+  /**
+  * Add movie to rest
+  * @return movie
+  */
+  //localhost:8080//movieRating/addMovie?title={title}&genre={genre}
+  @PostMapping(path = "movie")
+  public Movie addMovie(@RequestParam("title") String title, 
+      @RequestParam("genre") String genre) {
+    if (movSer.ableToCreateMovie(title, genre)) {
+      return movSer.getMovie(title, genre);
+    } //hvordan sjekke om film allerede fins?
+    throw new IllegalArgumentException("Invalid movie");
+  }
+  
+  /**
+  * Add rating to movie in rest
+  * @return movie
+  */
+  //localhost:8080//movieRating/addRating?movie={movie}&rating={rating}  
+  @PutMapping(path = "addRating")
+  public Movie addRatingToMovie(@RequestParam("movie") Movie movie, 
+  @RequestParam("rating") Integer rating) {
+    if (movSer.ableToRate(movie, rating)){
+      return new Movie("heis", "fantasy");
+    } //hvordan sjekke om filmen allerede fins
+    else {
+      throw new IllegalArgumentException("Not able to rate");
     }
+  }
 
-    // @PostMapping(path = "create-movie",
-    // consumes = MediaType.APPLICATION_JSON_VALUE)
-    // public void createAdminUser(@RequestBody Movie newMovie) {
-    //   movieRegister.addMovie(newMovie);
-    // }
-
-    // @PutMapping(path = "users")
-    // public List<Movie> getUserRegister() {
-    //     return movieRegister.updateMovieList();
-    // }
-
-    // // get all the users from file
-    // @GetMapping(path = "getMovies")
-    // public List<Movie> getUsers(){
-    //     List<Movie> users = null;
-    //     return users;
-    // }
+  // get all the users from file
+  @RequestMapping(path = "getUsers")
+  public List<User> getUserRegister(){
+      List<User> users = this.getUserRegister();
+      return users;
+  }
 }
 
