0e492d856d0c5deead3eba07f53661eb2645f6e7
Rebekka Stangvik <rebekast@stud.ntnu.no>
2022-11-14 09:31:01 +0100

    Fixed NaN value when movie is not rated
    Changed getAverage rating to Double instead of double
    
    This fixes #185

--- movieRating/core/src/main/java/core/Movie.java
+++ movieRating/core/src/main/java/core/Movie.java
@@ -107,7 +107,7 @@ public class Movie {
    * @return a double average rating
    */
   @JsonIgnore
-  public double getAverageRating() { // calculates average of all ratings for this movie
+  public Double getAverageRating() { // calculates average of all ratings for this movie
     Integer sum = 0;
     for (int rating = 0; rating < allRatings.size(); rating++) {
       sum += allRatings.get(rating);
@@ -123,9 +123,17 @@ public class Movie {
    * @return a string with title, genre and average rating for the movie
    */
   public String toString() { 
-    return "" + this.getTitle() + "; " 
+    if(this.getAverageRating().isNaN()){
+      return "" + this.getTitle() + "; " 
+      + this.getGenre() + "; " 
+      + 0.00;
+    }
+    else{
+      return "" + this.getTitle() + "; " 
       + this.getGenre() + "; " 
       + String.format("%.2f", this.getAverageRating());
+    }
+
   }
 
   public void deleteMovie(int rating){
